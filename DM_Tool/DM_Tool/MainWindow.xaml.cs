f9d4d3c46278cf1f3a5eca58c4c30e8fcd4c9b43
Raymond Gulbrandsen <raymond.gulbr@gmail.com>
2014-05-05 14:04:29 +0200

    Initial mvvm

---
+++ DM_Tool/DM_Tool/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using DM_Tool.ViewModel;
+
+namespace DM_Tool
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		/// <summary>
+		/// Initializes a new instance of the MainWindow class.
+		/// </summary>
+		public MainWindow()
+		{
+			InitializeComponent();
+			Closing += (s, e) => ViewModelLocator.Cleanup();
+		}
+	}
+}
